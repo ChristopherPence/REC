a7a496fc8e5d954f4d0021ba3ba8d259a827567b
ChristopherPence <chris1599@live.com>
2019-02-02 15:11:19 -0500

    node.js load html file

--- examples/ourFirstNodeSite.js
+++ examples/ourFirstNodeSite.js
@@ -1,6 +1,26 @@
-var http = require('http');
+//THIS IS AN EXAMPLE FROM W3 SCHOOLS, THIS IS NOT OUR OWN CODE
+//We are using this to test how node.js works
 
+//load the required modules
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+//create the server
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Hello World');
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	console.log(q.pathname);
+	fs.readFile(filename, function(err, data){
+		//If there was an error loading the file
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404 Not Found");
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+
+	
 }).listen(8080);
