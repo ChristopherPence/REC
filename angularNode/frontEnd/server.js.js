161261ec8b914017501e702e71aee251608e66a3
FinneganMPike <33106706+FinneganMPike@users.noreply.github.com>
2019-02-22 10:58:50 -0500

    Start of frontend

---
+++ angularNode/frontEnd/server.js.js
@@ -0,0 +1,90 @@
+//THIS IS AN EXAMPLE FROM W3 SCHOOLS, THIS IS NOT OUR OWN CODE
+//Only part of this code is our own. We are using this file to learn
+//We are using this to test how node.js works
+
+//load the required modules
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+//create the server
+http.createServer(function(req, res){
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+  //check for blank url
+  if(q.pathname == "/"){
+    fs.readFile("./index.html", function(err, data){
+      //If there was an error loading the file
+      if(err){
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+      });
+  }
+	//por read the specified file from the system
+	fs.readFile(filename, function(err, data){
+		//If there was an error loading the file
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404 Not Found");
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+}).listen(8080);
+
+var contin = 0;
+
+
+
+
+
+
+
+function terminator()
+{
+  const readline = require('readline');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('$:', (killNode) => {
+    // TODO: Log the answer in a database
+    if(killNode == "k")
+    {
+      
+      return process.kill(process.pid);
+      console.log(`Terminated Node Server`);
+    }
+    else if(killNode == "help")
+    {
+      console.log(`Commands allowed:`);
+      console.log(`k for killing the node server`);
+      rl.close();
+      terminator();
+    }
+    else if(killNode != "")
+    {
+      console.log(`Error: Type "help" for nessesary commands.`);
+      rl.close();
+      terminator();
+    }
+    else
+    {
+      rl.close();
+      terminator();
+    }
+    rl.close();
+  });
+}
+
+terminator();
+
+
+///mnt/c/Users/pikef/documents/github/REC/examples
