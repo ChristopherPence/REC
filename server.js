084314e12f409e9cda3868630e493b259f26957f
ChristopherPence <chris1599@live.com>
2019-02-24 12:50:09 -0500

    Added image posting to imgur

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const parser = require("body-parser");
 const request = require("request");
@@ -8,6 +9,7 @@ const app = express();
 require('dotenv').load();
 const port = process.env.MAIN_PORT;
 
+//Start the server
 app.use('/resources', express.static(__dirname + '/resources'));
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/login-register.html');
@@ -15,3 +17,27 @@ app.get('/', function(req, res){
 app.listen(port, function(){
 	console.log('Server started on port: ' + port)
 });
+
+
+
+
+//Make an image upload
+var options = {
+	'method' : 'POST',
+	'hostname' : 'api.imgur.com',
+	'path' : '/3/image',
+	'headers' : {
+		'Authorization': 'Client-ID ' + process.env.CLIENT_ID
+	}
+};
+
+var req = https.request(options, function(res) {
+	res.on('data', function(i){
+		console.log(i.toString());
+	}); 
+});
+
+// var postData = "https://raw.githubusercontent.com/ChristopherPence/REC/master/REC%20Flowchart.png";
+
+// req.write(postData);
+// req.end();
