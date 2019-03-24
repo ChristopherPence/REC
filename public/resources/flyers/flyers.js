42f7cf230efd7fde62a0b36975496b216ce17343
FinneganMPike <33106706+FinneganMPike@users.noreply.github.com>
2019-03-24 03:17:08 -0400

    Flyer Scroll
    I have made the flyer page side scrollable and changed some minor css things with the  nav bar

--- public/resources/flyers/flyers.js
+++ public/resources/flyers/flyers.js
@@ -1,14 +1,37 @@
+var numFlyers;
+var num;
 window.onresize = imageChange;
 
 function imageChange() 
 {
-
-  var widthz = window.innerWidth;
-  if(widthz > 800)
+  var widthz =  parseInt($('html').css('width'));
+  var heightz = parseInt($('html').css('height'));;
+  var widthImg = heightz * 0.75;
+  
+  if(widthImg > widthz*.99)
   {
-    widthz = 780;
+    widthImg = widthz * 0.99;
   }
-  $(".flyerz").css("width", widthz*0.9);
+  
+  $(".flyerz").css("height", widthImg);
+  $(".sideScroll").css("height", widthImg);
+  $(".sideScroll").css("width", widthImg);
+  
+  $('.horizontal-scroll-wrapper').css('height',widthz);
+  $('.horizontal-scroll-wrapper').css('width',widthImg+30);
+  $(".squares:first-child").css("padding-top", (widthz*.5  + widthImg*.5));
+  
+  $(".horizontal-scroll-wrapper").css("transform", "rotate(-90deg) translateY(-" + (widthImg+30)  + "px)");
+
+  $('.sideScroll').css('transform','rotate(90deg)');
+  $('.sideScroll').css('transform-origin','right top');
+    
+   numFlyers = $('.sideScroll').length;
+   num = widthImg;
+   var width = (numFlyers-1) * (num); 
+  $('.help').css('height',(width));
+  $(".help").css("margin-bottom", (widthz*.5  - widthImg*.5));
+  $('.flyerz').css('margin-top',10);
 }
 
 
@@ -26,7 +49,6 @@ var app = angular.module('myApp', []);
           });    
     $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) 
     {
-	     
       imageChange();
     });
   });
