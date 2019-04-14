2061240c232e71e7706f961772a4d252f7f62042
Steven Li <stevenli44@hotmail.com>
2019-04-14 17:53:21 -0400

    Added nav bar to the login and registration page

--- public/resources/news/news.js
+++ public/resources/news/news.js
@@ -37,31 +37,31 @@ function imageChange()
 }
 
 var app = angular.module('myApp', []);
-  app.controller('customersCtrl', function($scope, $http) {
-      $scope.run = function(){
-          console.log("Detected properly");
-          $http({
-              method : "get",
-              url : '/getnews/?page='+pageNumber+'&size='+size+'&search='+query,
-              data: {
-                  page: pageNumber,
-                  size: size,
-                  search: query
-              }
-          }).then(function mySuccess(response)
-          {
-              $scope.news = response.data;
-          }, function myError(response)
-          {
-              console.log(response);
-          });
+app.controller('customersCtrl', function($scope, $http) {
+    $scope.run = function(){
+        console.log("Detected properly");
+        $http({
+            method : "get",
+            url : '/getnews/?page='+pageNumber+'&size='+size+'&search='+query,
+            data: {
+                page: pageNumber,
+                size: size,
+                search: query
+            }
+        }).then(function mySuccess(response)
+        {
+            $scope.news = response.data;
+        }, function myError(response)
+        {
+            console.log(response);
+        });
 
-          $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent)
-          {
-              imageChange();
-          });
-      }
-  });
+        $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent)
+        {
+            imageChange();
+        });
+    }
+});
    
 app.filter('removeSpaces', [function() {
     return function(string) {
