f4cb8a646bbb8e4f6e3a13c35aecf8886afb743a
Carlos Power <chefpower25@gmail.com>
2019-04-14 17:54:36 -0400

    baking these cookies

--- public/resources/navbar/navbar.js
+++ public/resources/navbar/navbar.js
@@ -1,5 +1,5 @@
 var lecookie = angular.module('navbar', ['ngCookies']);
-    lecookie.controller('cookieController', ['$cookies', function($cookies) {
+    lecookie.controller('cookieCtrl', ['$cookies', function($cookies) {
         // Retrieving a cookie
         var favoriteCookie = $cookies.get('user');
         console.log(favoriteCookie);
