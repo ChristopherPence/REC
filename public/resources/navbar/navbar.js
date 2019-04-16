76eb2670c36036e222518c9f966d605398018621
Carlos Power <chefpower25@gmail.com>
2019-04-16 12:39:43 -0400

    fixing the oven

--- public/resources/navbar/navbar.js
+++ public/resources/navbar/navbar.js
@@ -1,8 +1,8 @@
-var lecookie = angular.module('navbar', ['ngCookies']);
-    lecookie.controller('cookieCtrl', ['$cookies', function($cookies) {
-        // Retrieving a cookie
-        console.log("Hi");
-        var favoriteCookie = $cookies.get('user');
-        console.log(favoriteCookie);
-    }]);
+// var lecookie = angular.module('navbar', ['ngCookies']);
+//     lecookie.controller('cookieCtrl', ['$cookies', function($cookies) {
+//         // Retrieving a cookie
+//         console.log("Hi");
+//         var favoriteCookie = $cookies.get('user');
+//         console.log(favoriteCookie);
+//     }]);
 
