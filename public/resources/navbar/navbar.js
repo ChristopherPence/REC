7a7c79d0ed583634adf81405ce35e78c1d440799
ChristopherPence <chris1599@live.com>
2019-04-16 13:15:08 -0400

    cookie bug

--- public/resources/navbar/navbar.js
+++ public/resources/navbar/navbar.js
@@ -1,8 +1,8 @@
-// var lecookie = angular.module('navbar', ['ngCookies']);
-//     lecookie.controller('cookieCtrl', ['$cookies', function($cookies) {
-//         // Retrieving a cookie
-//         console.log("Hi");
-//         var favoriteCookie = $cookies.get('user');
-//         console.log(favoriteCookie);
-//     }]);
+console.log('test');
+angular.module('myApp', ['ngCookies']).controller('cookieCtrl', ['$cookies', function($cookies) {
+    // Retrieving a cookie
+    console.log("Hi");
+    var favoriteCookie = $cookies.get('user');
+    console.log(favoriteCookie);
+}]);
 
