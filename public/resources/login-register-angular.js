c06476d8493958abffaf1d2dbcef233d038fa17a
Steven Li <stevenli44@hotmail.com>
2019-04-18 17:34:47 -0400

    Changes to the password verification with regex

--- public/resources/login-register-angular.js
+++ public/resources/login-register-angular.js
@@ -56,6 +56,12 @@ app.controller("mainController", function($scope, $http){
       $scope.registerError = "Passwords do not match!";
       return false;
     }
+    
+    // Check if there are any problems in inputting information for registration
+    if(checkPasswordRequirements() == false) {
+      $scope.registerError = "Please make sure your information meets all requirements!";
+      return false;
+    }
 
     // Hiding the modal after creating the account to allow the user to log in
     $("#registerModal").modal("hide");
@@ -97,70 +103,69 @@ app.controller("mainController", function($scope, $http){
   }
 });
 
-// Function to dynmaically display whether or not an email is valid
-function checkEmail() {
-  var getEmail = document.getElementById("registerEmail").value;
-  if(getEmail != ""){
-    $.ajax({
-      type: "GET",
-      url: "http://apilayer.net/api/check?access_key=2d7335eb56d000137084447e6443bb0e&email=" + getEmail + "&smtp=1&format=1",
-      dataType: "JSONP",
-      success: function(response) {
-        if(response.format_valid != true) {
-          document.getElementById("goodEmail").style.color = 'red';
-          document.getElementById("goodEmail").innerHTML = "Email is not in correct format!";
-          return false;
-        }
-
-        else if(response.domain != "rpi.edu") {
-          document.getElementById("goodEmail").style.color = 'red';
-          document.getElementById("goodEmail").innerHTML = "Email is not from the rpi.edu domain!";
-          return false;
-        }
-        else if(response.smtp_check != true) {
-          document.getElementById("goodEmail").style.color = 'red';
-          document.getElementById("goodEmail").innerHTML = "Email is not valid!";
-          return false;
-        }
-        else {
-          document.getElementById("goodEmail").style.color = 'green';
-          document.getElementById("goodEmail").innerHTML = "Valid Email!";
-          return true;
-        }
-      },
-      error: function(error) {
-        alert("There was a problem with: " + error);
-      }
-    });
-  }
-  else {
-    document.getElementById("goodEmail").innerHTML = "";
-  }
-}
-
-// Function to dynamically display whether or not password and confirm password fields are matching
-function checkPasswordMatch() {
-  if(document.getElementById('registerPassword').value == document.getElementById('confirmPassword').value && document.getElementById('registerPassword').value != "" && document.getElementById('confirmPassword').value != ""){
-    document.getElementById('matchingPassword').style.color = 'green';
-    document.getElementById('matchingPassword').innerHTML = "Passwords are matching!";
-  }
-  else if(document.getElementById('registerPassword').value == "" && document.getElementById('confirmPassword').value == ""){
-    document.getElementById('matchingPassword').innerHTML = "";
-  }
-  else {
-    document.getElementById('matchingPassword').style.color = 'red';
-    document.getElementById('matchingPassword').innerHTML = "Passwords are NOT matching!";
-  }
-}
+//// Function to dynmaically display whether or not an email is valid
+//function checkEmail() {
+//  var emailReq = true;
+//  console.log("HELLO");
+//  var getEmail = document.getElementById("registerEmail").value;
+//  if(getEmail != ""){
+//    $.ajax({
+//      type: "GET",
+//      url: "http://apilayer.net/api/check?access_key=2d7335eb56d000137084447e6443bb0e&email=" + getEmail + "&smtp=1&format=1",
+//      dataType: "JSONP",
+//      success: function(response) {
+//        console.log(response)
+//        if(response.format_valid != true) {
+//          document.getElementById("goodEmail").style.color = 'red';
+//          document.getElementById("goodEmail").innerHTML = "Email is not in correct format!";
+//          emailReq = false;
+//        }
+//
+//        else if(response.domain != "rpi.edu") {
+//          document.getElementById("goodEmail").style.color = 'red';
+//          document.getElementById("goodEmail").innerHTML = "Email is not from the rpi.edu domain!";
+//          emailReq = false;
+//        }
+//        else if(response.smtp_check != true) {
+//          document.getElementById("goodEmail").style.color = 'red';
+//          document.getElementById("goodEmail").innerHTML = "Email is not valid!";
+//          emailReq = false;
+//        }
+//        else {
+//          document.getElementById("goodEmail").style.color = 'green';
+//          document.getElementById("goodEmail").innerHTML = "Valid Email!";
+//        }
+//      },
+//      error: function(error) {
+//        alert("There was a problem with: " + error);
+//        emailReq = false;
+//      }
+//    });
+//  }
+//  else {
+//    document.getElementById("goodEmail").innerHTML = "";
+//    emailReq = false;
+//  }
+//  
+//  return emailReq;
+//}
 
 // Summation of all password checks that need to be done
 function checkPasswordRequirements() {
+  var lengthReq;
+  var specialReq;
+  var upperReq;
+  var lowerReq;
+  var digitReq;
+  var matchReq;
+  
   if(document.getElementById("registerPassword").value != ""){
-    checkPasswordLength();
-    checkPasswordSpecialChar();
-    checkPasswordUppercase();
-    checkPasswordLowercase();
-    checkPasswordDigits();
+    lengthReq = checkPasswordLength();
+    specialReq = checkPasswordSpecialChar();
+    upperReq = checkPasswordUppercase();
+    lowerReq = checkPasswordLowercase();
+    digitReq = checkPasswordDigits();
+    matchReq = checkPasswordMatch();
   }
   else {
     document.getElementById("lengthRequirement").innerHTML = "";
@@ -168,6 +173,28 @@ function checkPasswordRequirements() {
     document.getElementById("upperCaseRequirement").innerHTML = "";
     document.getElementById("lowerCaseRequirement").innerHTML = "";
     document.getElementById("digitRequirement").innerHTML = "";
+    document.getElementById('matchingPassword').innerHTML = "";
+    return false;
+  }
+  
+  return (lengthReq && specialReq && upperReq && lowerReq && digitReq && matchReq);
+}
+
+// Function to dynamically display whether or not password and confirm password fields are matching
+function checkPasswordMatch() {
+  if(document.getElementById('registerPassword').value == document.getElementById('confirmPassword').value && document.getElementById('registerPassword').value != "" && document.getElementById('confirmPassword').value != ""){
+    document.getElementById('matchingPassword').style.color = 'green';
+    document.getElementById('matchingPassword').innerHTML = "Passwords are matching!";
+    return true;
+  }
+  else if(document.getElementById('registerPassword').value == "" && document.getElementById('confirmPassword').value == ""){
+    document.getElementById('matchingPassword').innerHTML = "";
+    return false;
+  }
+  else {
+    document.getElementById('matchingPassword').style.color = 'red';
+    document.getElementById('matchingPassword').innerHTML = "Passwords are NOT matching!";
+    return false;
   }
 }
 
@@ -176,22 +203,26 @@ function checkPasswordLength() {
   if(document.getElementById("registerPassword").value.length >= 8){
     document.getElementById("lengthRequirement").style.color = "green";
     document.getElementById("lengthRequirement").innerHTML = "(1) Password meets minimum length of 8 characters!";
+    return true;
   }
   else {
     document.getElementById("lengthRequirement").style.color = "red";
     document.getElementById("lengthRequirement").innerHTML = "(1) Password does NOT meet minimum length of 8 characters!";
+    return false;
   }
 }
 
 // Function that checks that password of registration has a special character
 function checkPasswordSpecialChar() {
-  if(/[!@#$%^&*]/.test(document.getElementById("registerPassword"))){
+  if(/[!@#$%^&*]/.test(document.getElementById("registerPassword").value)){
     document.getElementById("specialCharRequirement").style.color = "green";
     document.getElementById("specialCharRequirement").innerHTML = "(2) Password must contain special character!";
+    return true;
   }
   else {
     document.getElementById("specialCharRequirement").style.color = "red";
     document.getElementById("specialCharRequirement").innerHTML = "(2) Password must contain special character!";
+    return false;
   }
 }
 
@@ -200,10 +231,12 @@ function checkPasswordUppercase() {
   if(/[A-Z]/.test(document.getElementById("registerPassword").value)){
     document.getElementById("upperCaseRequirement").style.color = "green";
     document.getElementById("upperCaseRequirement").innerHTML = "(3) Password must contain upper letter!";
+    return true;
   }
   else {
     document.getElementById("upperCaseRequirement").style.color = "red";
     document.getElementById("upperCaseRequirement").innerHTML = "(3) Password must contain uppercase letter!";
+    return false;
   }
 }
 
@@ -213,10 +246,12 @@ function checkPasswordLowercase() {
   if(/[a-z]/.test(document.getElementById("registerPassword").value)){
     document.getElementById("lowerCaseRequirement").style.color = "green";
     document.getElementById("lowerCaseRequirement").innerHTML = "(4) Password must contain lowercase letter!";
+    return true;
   }
   else {
     document.getElementById("lowerCaseRequirement").style.color = "red";
     document.getElementById("lowerCaseRequirement").innerHTML = "(4) Password must contain lowercase letter!";
+    return false;
   }
 }
 
@@ -226,10 +261,12 @@ function checkPasswordDigits() {
   if(/[0-9]/.test(document.getElementById("registerPassword").value)){
     document.getElementById("digitRequirement").style.color = "green";
     document.getElementById("digitRequirement").innerHTML = "(5) Password must contain digit!";
+    return true;
   }
   else {
     document.getElementById("digitRequirement").style.color = "red";
     document.getElementById("digitRequirement").innerHTML = "(5) Password must contain digit!";
+    return false;
   }
 }
 
