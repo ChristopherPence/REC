86c908588cddf4851b96f154deaef9f1264fed1a
Carlos Power <chefpower25@gmail.com>
2019-04-19 00:37:24 -0400

    im crying

--- public/resources/navbar/navbar.js
+++ public/resources/navbar/navbar.js
@@ -11,7 +11,7 @@ app.controller('cookieCtrl', ['$cookies', '$scope', '$sce', function($cookies, $
     }
     else{
         console.log('logged out');
-        $scope.html = '<a href = "profile.html" class = "nav right rounded">Profile</a>' + '<form method="post" action="/logout"> <input type="submit" value="Log Out" class="form-inline my-2 my-lg-0"></input></form>';
+        $scope.html = '<span class="row"><a href = "profile.html" class = "nav right rounded">Profile</a>' + '<form method="post" action="/logout" class="form-inline my-2 my-lg-0"> <input type="submit" value="Log Out" class="form-control mr-sm-2"> </input></form></span>';
         $scope.trustedHtml = $sce.trustAsHtml($scope.html);
     }
 }]);
